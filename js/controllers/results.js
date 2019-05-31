2c0461746f3bb2b2895d4b58522ccff8e82e583a
Jojo <41641896+jojov92@users.noreply.github.com>
2019-05-31 11:09:04 +0100

    added go back to main page button and reset quiz using function reset

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -11,6 +11,7 @@
         vm.DataService = DataService;
         vm.getAnswerClass = getAnswerClass;
         vm.setActiveQuestion = setActiveQuestion;
+        vm.reset = reset;
         vm.calculatePerc = calculatePerc;
         vm.activeQuestion = 0;
 
@@ -31,6 +32,18 @@
                 return "bg-success";
             }else if(index === DataService.quizQuestions[vm.activeQuestion].selected) {
                 return "bg-danger";
+            }
+        }
+
+        function reset(){
+            quizMetrics.changeState("results", false);
+            quizMetrics.numCorrect = 0;
+
+            for(var i = 0; i < DataService.quizQuestions.length; i++) {
+                var data = DataService.quizQuestions[i];
+
+                data.selected = null;
+                data.correct = null;
             }
         }
     }
