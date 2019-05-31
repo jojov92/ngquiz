380fb6e20cd8a2c777ab2a7b3bdaddfc9b52ab83
Jojo <41641896+jojov92@users.noreply.github.com>
2019-05-31 10:12:52 +0100

    highlighted correct and incorrect answers

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -9,5 +9,19 @@
 
         vm.quizMetrics = quizMetrics;
         vm.DataService = DataService;
+        vm.getAnswerClass = getAnswerClass;
+        vm.activeQuestion = 0;
+
+
+        /** checking if the index of that point in the loop (index line 216) is equal to the correct answers index and 
+        if it is then it will return the class bg-success, but if it's an answer that's incorrect, it will return bg-danger class*/
+        function getAnswerClass(index){
+            
+            if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
+                return "bg-success";
+            }else if(index === DataService.quizQuestions[vm.activeQuestion].selected) {
+                return "bg-danger";
+            }
+        }
     }
 })();
